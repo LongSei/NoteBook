cfa63da79ac558acab88bb5ecaad6819a6e8fd4c
Long_Sei <htaevd@gmail.com>
2022-12-16 00:47:11 +0700

    Add Combinator.h

---
+++ Combinator.h
@@ -0,0 +1,13 @@
+int fact[MAX], inv[MAX];
+
+void init(int n) {
+    fact[0] = inv[0] = 1;
+	for (int i = 1; i <= n; i++) {
+		fact[i] = 1LL * fact[i - 1] * i % mod;
+		inv[i] = fast_pow(fact[i], mod - 2);
+	}
+}
+int C(int k, int n) {
+	if (n < k) return 0;
+	return 1LL * fact[n] * inv[k] % mod * inv[n - k] % mod;
+}

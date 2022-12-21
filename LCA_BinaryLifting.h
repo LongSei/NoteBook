1837950e9035b658e7a0e06fca7883606fba85da
Long_Sei <htaevd@gmail.com>
2022-12-21 17:24:53 +0700

    Update LCA

--- LCA_BinaryLifting.h
+++ LCA_BinaryLifting.h
@@ -1,33 +1,37 @@
-int tin[MAX], tout[MAX], timer = 0, up[MAX][21];
-void dfs(int v, int p)
-{
-    tin[v] = ++timer;
+// Preparing and finding "Lowest Common Ancestor"
+// Time complexity: Prepare: O(NlogN), Finding: O(logN)
+// Tested: 
+// - https://oj.vnoi.info/problem/pwalk
+
+int tin[MAX], tout[MAX], lcaTimer = 0, up[MAX][21];
+vector<int> adjlist[MAX];
+void dfs(int v, int p) {
+    tin[v] = ++lcaTimer;
     up[v][0] = p;
     for (int i = 1; i <= ceil(log2(n)); ++i)
         up[v][i] = up[up[v][i-1]][i-1];
 
-    for (int u : adj[v]) {
-        if (u != p)
+    for (int u : adjlist[v]) {
+        if (u != p) {
             dfs(u, v);
+        }
     }
-
-    tout[v] = ++timer;
+    tout[v] = ++lcaTimer;
 }
 
-bool is_ancestor(int u, int v)
-{
+bool is_ancestor(int u, int v) {
     return tin[u] <= tin[v] && tout[u] >= tout[v];
 }
 
-int lca(int u, int v)
-{
+int LCA(int u, int v) {
     if (is_ancestor(u, v))
         return u;
     if (is_ancestor(v, u))
         return v;
     for (int i = ceil(log2(n)); i >= 0; --i) {
-        if (!is_ancestor(up[u][i], v))
+        if (!is_ancestor(up[u][i], v)) {
             u = up[u][i];
+        }
     }
     return up[u][0];
 }
